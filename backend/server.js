8e89eaf5656510af2cf73aa0eb2d5ca821ad86a4
Marvin <marvinluckyanto123@gmail.com>
2021-11-23 23:34:28 +0700

    Fixed User Model

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,31 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 // Initialization
+
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Config
-dotenv.config();
-const port = process.env.PORT || 5000;
-const db = process.env.DB_URI;
+const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
+
 mongoose
-  .connect(db, { useNewUrlParser: true, connectTimeoutMS: 5000 })
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 5000,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 // Routes
-import userRouter from "./routes/user.js";
-app.use("/user", userRouter);
+import userRouter from "./routes/userRouter.js";
+app.use("/auth", userRouter);
 
 // App listener
-app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+
+app.listen(PORT, () => {
+  console.log(`Started on port ${PORT}`);
 });
