83f34227ab79d20c0bac3b5f9a767321d6569754
devarahifz <devara.hifzhurrahman22@gmail.com>
2021-12-09 12:45:45 +0700

    Update Router

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,32 +1,39 @@
 import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./components/auth/Register";
+import Register from "./pages/Register";
 import Navbar from "./components/layout/Navbar";
-import Login from "./components/auth/Login";
-import Home from "./components/Home";
-import Items from "./components/items/Items";
+import Footer from "./components/layout/Footer";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Items from "./components/old/items/Items";
 import AuthContext from "./context/AuthContext";
+import './index.css'
 
 export default function Router() {
   const { loggedIn } = useContext(AuthContext);
 
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        {loggedIn === false && (
-          <>
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/login" element={<Login />} />
-          </>
-        )}
-        {loggedIn === true && (
-          <>
-            <Route exact path="/item" element={<Items />} />
-          </>
-        )}
-      </Routes>
+      <div className="main-app-wrapper">
+        <Navbar />
+        <div className="content-wrapper">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          {loggedIn === false && (
+            <>
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/login" element={<Login />} />
+            </>
+          )}
+          {loggedIn === true && (
+            <>
+              <Route exact path="/item" element={<Items />} />
+            </>
+          )}
+        </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
